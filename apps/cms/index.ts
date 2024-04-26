9760ea7dece69bdac64cc7f2813c5755f9c81706
trungpd1102 <156633588+trungphammycolor@users.noreply.github.com>
2024-04-26 14:15:57 +0700

    :sparkles: init cloudflare

---
+++ apps/cms/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaD1 } from '@prisma/adapter-d1';
+
+export interface Env {
+  DB: D1Database;
+}
+
+export default {
+  async fetch(request, env, ctx): Promise<Response> {
+    console.log({ env });
+
+    const adapter = new PrismaD1(env.DB);
+    const prisma = new PrismaClient({ adapter });
+
+    const users = await prisma.userRole.findMany();
+    const result = JSON.stringify(users);
+    console.log({ result });
+    return new Response(result);
+  },
+} satisfies ExportedHandler<Env>;
--- apps/cms/src/app/page.tsx
+++ apps/cms/src/app/page.tsx
@@ -3,7 +3,12 @@ import dynamic from 'next/dynamic';
 const AdminApp = dynamic(() => import('@/views/AdminApp'), {
   ssr: false,
 });
+// Config run time to Edge
+// https://developers.cloudflare.com/pages/framework-guides/nextjs/deploy-a-nextjs-site/
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#runtime
+export const runtime = 'edge';
 
 const Home: NextPage = () => <AdminApp />;
 
 export default Home;
+
