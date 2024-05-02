bb1d0e7fe63a2e59a312a689a76d3513cf9b00f0
trungpd1102 <156633588+trungphammycolor@users.noreply.github.com>
2024-05-02 09:56:23 +0700

    :sparkles: revert term of use call back

--- apps/cms/src/providers/dataProviders/termOfUseManagementCallbackHandlers.ts
+++ apps/cms/src/providers/dataProviders/termOfUseManagementCallbackHandlers.ts
@@ -1,19 +1,75 @@
+// import type { DataProvider, GetListResult, GetOneResult } from 'react-admin';
+
+// import dayjs from 'dayjs';
+// const termOfUseManagementCallbackHandlers = {
+//   resource: 'term-of-uses',
+
+//   afterGetOne: async (
+//     response: GetOneResult,
+//     dataProvider: DataProvider
+//   ): Promise<GetOneResult> => {
+//     const { content } = response.data;
+
+//     const object = await dataProvider.getObject({ key: content });
+//     response.data.content = object.data.body;
+
+//     return response;
+//   },
+// };
+
+// export default termOfUseManagementCallbackHandlers;
+
 import type { DataProvider, GetListResult, GetOneResult } from 'react-admin';
 
 import dayjs from 'dayjs';
 const termOfUseManagementCallbackHandlers = {
-  resource: 'term-of-uses',
+  resource: 'term-of-use-managements',
+
+  afterGetList: async (
+    response: GetListResult,
+    dataProvider: DataProvider
+  ): Promise<GetListResult> => {
+    // const { classificationId } = response.data;
+
+    let fake = [
+      {
+        id: '1',
+        termOfUseId: '3',
+        memo: 'duong abc',
+        version: '1',
+        performanceTypeId: '2',
+        dateStart: dayjs(new Date()).format('YYYY.MM.DD HH:mm'),
+        dateRegistration: dayjs(new Date()).format('YYYY.MM.DD HH:mm'),
+        fileConditionTerms: 'tearn-of-use.html',
+      },
+    ];
+
+    return {
+      data: fake,
+      total: 1,
+    };
+  },
 
   afterGetOne: async (
     response: GetOneResult,
     dataProvider: DataProvider
   ): Promise<GetOneResult> => {
-    const { content } = response.data;
+    // const { classificationId } = response.data;
 
-    const object = await dataProvider.getObject({ key: content });
-    response.data.content = object.data.body;
+    let fake = {
+      id: '1',
+      termOfUseId: '3',
+      memo: 'duong abc',
+      version: '1',
+      performanceTypeId: '2',
+      dateStart: dayjs(new Date()).format('YYYY.MM.DD HH:mm'),
+      dateRegistration: dayjs(new Date()).format('YYYY.MM.DD HH:mm'),
+      fileConditionTerms: 'tearn-of-use.html',
+    };
 
-    return response;
+    return {
+      data: fake,
+    };
   },
 };
 
