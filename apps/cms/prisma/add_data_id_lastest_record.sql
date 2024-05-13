8a8f4a3a466d9c602289a9eea05bdfc4cec05f74
duongMyColor <165616663+duongMyColor@users.noreply.github.com>
2024-05-13 17:26:37 +0700

    :+1: fix:fix file sql

--- apps/cms/prisma/add_data_id_lastest_record.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO IdLastestOfRecord (name,idLastest,updatedAt) VALUES 
-('acstaManagement', 1, CURRENT_TIMESTAMP),
-('performaceManagement', 1, CURRENT_TIMESTAMP),
-('license', 1, CURRENT_TIMESTAMP),
-('termsOfUse', 1, CURRENT_TIMESTAMP),
-('forcedUpdateManagements', 1, CURRENT_TIMESTAMP)
