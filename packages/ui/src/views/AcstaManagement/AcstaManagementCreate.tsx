2511f27668f114053be7bb5718699733f042b6f0
duongMyColor <165616663+duongMyColor@users.noreply.github.com>
2024-05-14 15:13:48 +0700

    fix: acsta/ID auto increase

--- packages/ui/src/views/AcstaManagement/AcstaManagementCreate.tsx
+++ packages/ui/src/views/AcstaManagement/AcstaManagementCreate.tsx
@@ -54,8 +54,10 @@ const AcstaManagementCreate = ({
 
   useEffect(() => {
     const fetchIdLastest = async () => {
-      const response = await dataProvider.getIdLastest(resource);
-      const nextId = response.data.length > 0 ? response.data[0].id + 1 : 1;
+      const response = await dataProvider.getIdLastestRecord(resource);
+
+      console.log({ response });
+      const nextId = response.data?.idLastest ? response.data.idLastest : 1;
       setIdAcsta(`${nextId}`);
     };
 
