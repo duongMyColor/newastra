91e32a0f888f45a11af6f866f492a9082afe5b4f
trungphammycolor <trung.pham@mycolor.biz>
2024-05-21 09:01:40 +0700

    :recycle: rename auth provider

--- apps/cms/src/providers/authProvider.ts
+++ apps/cms/src/providers/authProvider.ts
@@ -9,118 +9,112 @@ const httpClient = fetchUtils.fetchJson;
 
 const authProvider: AuthProvider = {
   // authentication
-  // login: async (params) => {
-  //   const { username, password } = params;
-  //   const request = new Request(`${apiUrl}/access/login`, {
-  //     method: 'POST',
-  //     body: JSON.stringify({ username, password }),
-  //     // headers: new Headers({ 'Content-Type': 'application/json' }),
-  //   });
-
-  //   try {
-  //     const { json } = await httpClient(request);
-
-  //     const {
-  //       metadata: {
-  //         user: { id },
-  //         tokens: { accessToken, refreshToken },
-  //       },
-  //     } = json;
-
-  //     // Set cookie
-  //     document.cookie = `${HEADER.CLIENT_ID}=${id}; path=/`;
-  //     document.cookie = `${HEADER.AUTHORIZATION}=${accessToken}; path=/`;
-  //     document.cookie = `${HEADER.REFRESHTOKEN}=${refreshToken}; path=/`;
-
-  //     return Promise.resolve();
-  //   } catch (error) {
-  //     return Promise.reject(error);
-  //   }
-  // },
   login: async (params) => {
-    return Promise.resolve();
-  },
-  checkError: (error) => Promise.resolve(/* ... */),
-  checkAuth: async () => {
-    return Promise.resolve();
-  },
-  // checkAuth: async () => {
-  //   const request = new Request(`${apiUrl}/access/auth`, {
-  //     method: 'POST',
-  //   });
-
-  //   try {
-  //     const { json } = await httpClient(request);
-  //     console.log({ json });
-
-  //     return Promise.resolve();
-  //   } catch (error) {
-  //     return Promise.reject(error);
-  //   }
-  // },
-  logout: async () => {
-    return Promise.resolve();
-  },
-  // logout: async () => {
-  //   const request = new Request(`${apiUrl}/access/logout`, {
-  //     method: 'POST',
-  //   });
-
-  //   try {
-  //     const { json } = await httpClient(request);
-  //     console.log({ json });
-
-  //     return Promise.resolve();
-  //   } catch (error) {
-  //     console.log({ error });
-  //   }
-  // },
-  getIdentity: async () => {
-    return Promise.resolve({
-      id: '1',
-      fullName: 'admin',
+    const { username, password } = params;
+    const request = new Request(`${apiUrl}/access/login`, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      // headers: new Headers({ 'Content-Type': 'application/json' }),
     });
+
+    try {
+      const { json } = await httpClient(request);
+
+      const {
+        metadata: {
+          user: { id },
+          tokens: { accessToken, refreshToken },
+        },
+      } = json;
+
+      // Set cookie
+      document.cookie = `${HEADER.CLIENT_ID}=${id}; path=/`;
+      document.cookie = `${HEADER.AUTHORIZATION}=${accessToken}; path=/`;
+      document.cookie = `${HEADER.REFRESHTOKEN}=${refreshToken}; path=/`;
+
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
-  // getIdentity: async () => {
-  //   const userId = getClientCookieValue(HEADER.CLIENT_ID);
 
-  //   const request = new Request(`${apiUrl}/users/${userId}`, {
-  //     method: 'GET',
-  //   });
+  checkError: (error) => {
+    const status = error.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('auth');
+      return Promise.reject({ message: 'Unauthorized user!' });
+    }
+    // other error code (404, 500, etc): no need to log out
+    return Promise.resolve();
+  },
 
-  //   try {
-  //     const {
-  //       json: {
-  //         metadata: { id, name },
-  //       },
-  //     } = await httpClient(request);
+  checkAuth: async () => {
+    const request = new Request(`${apiUrl}/access/auth`, {
+      method: 'POST',
+    });
 
-  //     return Promise.resolve({
-  //       id,
-  //       fullName: name,
-  //     });
-  //   } catch (error) {
-  //     return Promise.reject(error);
-  //   }
-  // },
+    try {
+      const { json } = await httpClient(request);
+      console.log({ json });
+
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+
+  logout: async () => {
+    const request = new Request(`${apiUrl}/access/logout`, {
+      method: 'POST',
+    });
+
+    try {
+      const { json } = await httpClient(request);
+      console.log({ json });
+
+      return Promise.resolve();
+    } catch (error) {
+      console.log({ error });
+    }
+  },
+
+  getIdentity: async () => {
+    const userId = getClientCookieValue(HEADER.CLIENT_ID);
+
+    const request = new Request(`${apiUrl}/users/${userId}`, {
+      method: 'GET',
+    });
+
+    try {
+      const {
+        json: {
+          metadata: { id, username },
+        },
+      } = await httpClient(request);
+
+      return Promise.resolve({
+        id,
+        fullName: username,
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
 
   // authorization
   getPermissions: async () => {
-    return Promise.resolve(generateRole('ADMIN'));
+    const request = new Request(`${apiUrl}/access/permission`, {
+      method: 'GET',
+    });
+
+    try {
+      const { json } = await httpClient(request);
+
+      return Promise.resolve(generateRole(json.metadata.role));
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
-  // getPermissions: async () => {
-  //   const request = new Request(`${apiUrl}/access/permission`, {
-  //     method: 'GET',
-  //   });
-
-  //   try {
-  //     // const { json } = await httpClient(request);
-
-  //     return Promise.resolve(generateRole('ADMIN'));
-  //   } catch (error) {
-  //     return Promise.reject(error);
-  //   }
-  // },
 };
 
 export default authProvider;
