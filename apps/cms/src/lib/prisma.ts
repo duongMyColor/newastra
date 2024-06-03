11cccba23d1abbf3d021577b00fda973c9fd052a
trungpd1102 <156633588+trungphammycolor@users.noreply.github.com>
2024-06-03 17:16:34 +0700

    :+1: recycle: refactor prisma.ts

--- apps/cms/src/lib/prisma.ts
+++ apps/cms/src/lib/prisma.ts
@@ -2,52 +2,34 @@ import { PrismaD1 } from '@prisma/adapter-d1';
 import { PrismaClient } from '@prisma/client';
 import { getRequestContext } from '@cloudflare/next-on-pages';
 import { AsyncLocalStorage } from 'node:async_hooks';
+
 interface Env extends CloudflareEnv {
   DB: D1Database;
 }
 
-const localStorage: any = new AsyncLocalStorage();
+interface Storage {
+  [key: string]: PrismaClient;
+}
+
+const localStorage = new AsyncLocalStorage();
 
 const getStorageKey = (key: string) => {
-  const store = localStorage.getStore() as any;
+  const store = localStorage.getStore() as Storage;
+  return store ? store[key] : null;
+};
 
-  if (!store) {
-    return null;
-  }
-
-  return store[key];
+const createPrismaClient = () => {
+  const env = getRequestContext().env as Env;
+  const DB = env.DB;
+  const adapter = new PrismaD1(DB);
+  const prismaObj = new PrismaClient({ adapter });
+  localStorage.run({ prisma: prismaObj }, () => {});
+  return prismaObj;
 };
 
 const generateClient = () => {
-  if (!localStorage) {
-    const env = getRequestContext().env as Env;
-    const DB = env.DB;
-    const adapter = new PrismaD1(DB);
-    let prismaObj = new PrismaClient({
-      adapter,
-    });
-
-    localStorage.run({ prisma: prismaObj }, () => {});
-
-    return prismaObj;
-  }
-
-  if (getStorageKey('prisma')) {
-    return getStorageKey('prisma');
-  }
-
-  if (!getStorageKey('prisma')) {
-    const env = getRequestContext().env as Env;
-    const DB = env.DB;
-    const adapter = new PrismaD1(DB);
-    let prismaObj = new PrismaClient({
-      adapter,
-    });
-
-    localStorage.run({ prisma: prismaObj }, () => {});
-
-    return prismaObj;
-  }
+  const existingClient = getStorageKey('prisma');
+  return existingClient ? existingClient : createPrismaClient();
 };
 
 export { generateClient };
