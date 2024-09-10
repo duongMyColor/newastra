25103ec8f8cd760c87154200653514f67c305045
duongMyColor <165616663+duongMyColor@users.noreply.github.com>
2024-09-10 09:39:20 +0700

    fix: fix_reivew

--- apps/api/src/services/applicationMaster.sercvice.ts
+++ apps/api/src/services/applicationMaster.sercvice.ts
@@ -11,9 +11,8 @@ import {
   AplicationMasterPostIF,
   AplicationMasterResponseIF,
 } from '@repo/types/applicationMaster';
-import { getBundleId } from '@/lib/globalObject';
-import LicenseService from './license.service';
-import TermOfUseService from './termOfUse.service';
+import { getBundleId, getDb } from '@/lib/globalObject';
+
 class ApplicationMasterFactory {
   static async getAll() {
     const applicationMasters = await getAll();
@@ -33,22 +32,52 @@ class ApplicationMasterFactory {
 
   static async getOneByBundleId() {
     const bundleId = getBundleId();
-
+    const prisma = getDb();
     if (!bundleId) {
       throw new NotFoundError('bundleId not found');
     }
-    const [license, termOfUse, application] = await Promise.all([
-      LicenseService.getCurrentLicense(),
-      TermOfUseService.getCurrentTermOfUse(),
-      getOneByBundleId(bundleId),
-    ]);
+
+    const application = await getOneByBundleId(bundleId);
     if (!application) {
       return {};
     }
+    const licensePromise = prisma.license.findFirst({
+      where: {
+        id: application.licenseId,
+      },
+    });
+
+    const termsOfUsePromise = prisma.termsOfUse.findFirst({
+      where: {
+        id: application.termsOfUseId,
+      },
+    });
+
+    const [currentLicense, currentTermsOfUse] = await Promise.all([
+      licensePromise,
+      termsOfUsePromise,
+    ]);
+
+    if (!currentLicense) {
+      throw new NotFoundError('License not found');
+    }
+
+    if (!currentTermsOfUse) {
+      throw new NotFoundError('Term of use not found');
+    }
+
+    const [contentUrlLicense, contentUrlTermsOfUse] = await Promise.all([
+      getPresignedUrl(currentLicense.content as string),
+      getPresignedUrl(currentTermsOfUse.content as string),
+    ]);
 
     const dataApp = await new ApplicationMaster().convertData(application);
 
-    return { ...dataApp, license, termOfUse };
+    return {
+      ...dataApp,
+      license: { contentUrl: contentUrlLicense },
+      termOfUse: { contentUrl: contentUrlTermsOfUse },
+    };
   }
 
   static async getManyByIds(ids: number[]) {
@@ -62,22 +91,52 @@ class ApplicationMasterFactory {
 
   static async getUpdateData(lastSyncDate: Date | string) {
     const bundleId = getBundleId();
+    const prisma = getDb();
     if (!bundleId) {
       throw new NotFoundError('bundleId not found');
     }
-    const [license, termOfUse, app] = await Promise.all([
-      LicenseService.getCurrentLicense(),
-      TermOfUseService.getCurrentTermOfUse(),
-      getUpdateData(lastSyncDate, bundleId),
-    ]);
 
+    const app = await getUpdateData(lastSyncDate, bundleId);
     if (!app) {
       return {};
     }
+    const licensePromise = prisma.license.findFirst({
+      where: {
+        id: app.licenseId,
+      },
+    });
+
+    const termsOfUsePromise = prisma.termsOfUse.findFirst({
+      where: {
+        id: app.termsOfUseId,
+      },
+    });
+
+    const [currentLicense, currentTermsOfUse] = await Promise.all([
+      licensePromise,
+      termsOfUsePromise,
+    ]);
+
+    if (!currentLicense) {
+      throw new NotFoundError('License not found');
+    }
+
+    if (!currentTermsOfUse) {
+      throw new NotFoundError('Term of use not found');
+    }
+
+    const [contentUrlLicense, contentUrlTermsOfUse] = await Promise.all([
+      getPresignedUrl(currentLicense.content as string),
+      getPresignedUrl(currentTermsOfUse.content as string),
+    ]);
 
     const dataApp = await new ApplicationMaster().convertData(app);
 
-    return { ...dataApp, license, termOfUse };
+    return {
+      ...dataApp,
+      license: { contentUrl: contentUrlLicense },
+      termOfUse: { contentUrl: contentUrlTermsOfUse },
+    };
   }
 
   static async convertArrayData(apps: AplicationMasterResponseIF[]) {
